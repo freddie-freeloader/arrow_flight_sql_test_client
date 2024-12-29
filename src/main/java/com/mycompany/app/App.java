a332480e8d193c25b9c6a730ebf1f279c4b22802
Jonas Benn <code@benn.in>
2024-12-29 10:47:33 +0000

    Fix memory leak

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,21 +3,22 @@ package com.mycompany.app;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
-        // Use
-        // jdbc:arrow-flight-sql://localhost:31337?useEncryption=true&user=sqlflite_username&password=sqlflite_password&disableCertificateVerification=true
         try (Connection connection = DriverManager.getConnection(
-                "jdbc:arrow-flight-sql://sqlflite:31337?useEncryption=true&user=sqlflite_username&password=sqlflite_password&disableCertificateVerification=true")) {
+                "jdbc:arrow-flight-sql://sqlflite:31337?useEncryption=true&user=sqlflite_username&password=sqlflite_password&disableCertificateVerification=true");) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement
+                    .executeQuery("SELECT \"c_custkey\" FROM \"main\".\"customer\" LIMIT 10;");
             // Get status of the connection
             System.out.println("Connection status: " + connection.isClosed());
-            // Execute a query
-            connection.createStatement().executeQuery("SELECT \"c_custkey\" FROM \"main\".\"customer\" LIMIT 10;");
-            connection.close();
+            // Process the result set
+            while (resultSet.next()) {
+                System.out.println("c_custkey: " + resultSet.getInt("c_custkey"));
+            }
         } catch (SQLException e) {
             System.out.println("SQL Exception: " + e);
         }
